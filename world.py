f60ddbfa05dd5f50c3af216e0cee0a0e2259e108
CJenkins <c.e.jenkins001@gmail.com>
2024-05-01 12:57:52 +0100

    Player & Player movement
    A red rectangle, which is the player.
    
    Movement with WASD.

--- world.py
+++ world.py
@@ -5,5 +5,20 @@ class World:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
 
+        self.player = pygame.Rect(270,180,50,50)
+
     def run(self):
         self.display_surface.fill('grey')
+
+        pygame.draw.rect(self.display_surface, 'red', self.player)
+
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_a] == True:
+            self.player.x -= 5
+        if key[pygame.K_d] == True:
+            self.player.x += 5
+        if key[pygame.K_w] == True:
+            self.player.y -= 5
+        if key[pygame.K_s] == True:
+            self.player.y += 5
