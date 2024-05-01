9bc3f6b5bb3b992d6217d4f85c0b374c02187ef6
CJenkins <c.e.jenkins001@gmail.com>
2024-05-01 12:35:19 +0100

    Project Setup

---
+++ main.py
@@ -0,0 +1,31 @@
+from settings import *
+from world import World
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption("Collision Demo")
+
+        self.clock = pygame.time.Clock()
+
+        self.world_screen = World()
+        
+
+
+    def run(self):
+        while True:
+            FPS = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.QUIT()
+
+
+            self.world_screen.run()
+
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
